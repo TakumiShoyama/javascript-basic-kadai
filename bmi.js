d8c2e264ed0aeddca228e46b6b12060ded369732
TakumiShoyama <sho11071228@gmail.com>
2024-12-02 07:26:11 +0900

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,11 @@
+let weightNumber; 
+weightNumber = (68);
+
+let hightNumber; 
+hightNumber = (1.7);
+
+let bmiNumber;
+bmiNumber = (weightNumber / (hightNumber * hightNumber));
+
+console.log(bmiNumber);
+
